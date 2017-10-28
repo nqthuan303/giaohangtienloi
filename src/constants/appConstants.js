4a3913cc9fb3b65b9968d4bf0286aeedeebc8f3f
Thuan Nguyen <nqthuan303@gmail.com>
2017-10-28 23:38:34 +0700

    fix build

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,4 +1,4 @@
 const env = process.env.NODE_ENV;
 console.log(env);
-export const API_URL = (env === 'development') ? 'http://localhost:3435/api': 'https://ghtl-api.herokuapp.com/api' ;
-// export const API_URL = 'https://ghtl-api.herokuapp.com/api' ;
+// export const API_URL = (env === 'development') ? 'http://localhost:3435/api': 'https://ghtl-api.herokuapp.com/api' ;
+export const API_URL = 'https://ghtl-api.herokuapp.com/api';
